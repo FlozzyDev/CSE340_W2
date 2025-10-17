4d83bb281d57e5f5fe2c896fbde847295dfbc9ea
Dallin Hale <dallinhale@gmail.com>
2025-10-17 12:00:28 -0600

    changed things to classes sinc html errors showed up (duplicate IDs since cards duplicated).

--- utilities/index.js
+++ utilities/index.js
@@ -38,9 +38,9 @@ Util.buildClassificationGrid = async function (data) {
       "<p id='error-message'> Sorry, we appear to be out of stock of that type. Check back later!</p>";
     return grid;
   } else {
-    grid = '<ul id="inv-display">';
+    grid = '<ul class="inv-display">';
     data.forEach((vehicle) => {
-      grid += "<li id='inv-display-item'>";
+      grid += "<li class='inv-display-item'>";
       grid +=
         '<a href="../../inv/detail/' +
         vehicle.inv_id +
@@ -56,7 +56,7 @@ Util.buildClassificationGrid = async function (data) {
         vehicle.inv_model +
         ' on CSE Motors" ></a>';
       grid += '<div class="namePrice">';
-      grid += "<hr id='details-divider' >";
+      grid += "<hr class='details-divider' >";
       grid += "<h2>";
       grid +=
         '<a href="../../inv/detail/' +
@@ -93,8 +93,8 @@ Util.buildItemDetail = async function (data) {
     error.status = 404;
     throw error;
   } else {
-    detail = '<div id="container-item-details">';
-    detail += '<div id="item-details-image">';
+    detail = '<div class="container-item-details">';
+    detail += '<div class="item-details-image">';
     detail +=
       '<img src="' +
       data[0].inv_image +
@@ -103,28 +103,28 @@ Util.buildItemDetail = async function (data) {
       " " +
       data[0].inv_model +
       ' on CSE Motors" >';
-    detail += "<hr id='details-divider' >";
+    detail += "<hr class='details-divider' >";
     detail += "</div>";
-    detail += '<div id="item-details-details">';
-    detail += "<ul id='item-details-list'>";
+    detail += '<div class="item-details-details">';
+    detail += "<ul class='item-details-list'>";
     detail +=
-      "<li id='item-detail-price'><span class='item-header'>Price: </span>$" +
+      "<li class='item-detail-price'><span class='item-header'>Price: </span>$" +
       new Intl.NumberFormat("en-US").format(data[0].inv_price) +
       "</li>";
     detail +=
-      "<li id='item-detail-desc'><span class='item-header'>Description: </span>" +
+      "<li class='item-detail-desc'><span class='item-header'>Description: </span>" +
       data[0].inv_description +
       "</li>";
     detail +=
-      "<li id='item-detail-color'><span class='item-header'>Color: </span>" +
+      "<li class='item-detail-color'><span class='item-header'>Color: </span>" +
       data[0].inv_color +
       "</li>";
     detail +=
-      "<li id='item-detail-year'><span class='item-header'>Year: </span>" +
+      "<li class='item-detail-year'><span class='item-header'>Year: </span>" +
       data[0].inv_year +
       "</li>";
     detail +=
-      "<li id='item-detail-miles'><span class='item-header'>Miles: </span>" +
+      "<li class='item-detail-miles'><span class='item-header'>Miles: </span>" +
       new Intl.NumberFormat("en-US").format(data[0].inv_miles) +
       "</li>";
     detail += "</ul>";
